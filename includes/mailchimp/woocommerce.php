35bd578e833ae491f3de0c4588dac3d9b753f15e
Serge Degtiarev <ser6722@yandex.ru>
2025-06-16 11:42:39 +1200

    wip MC add to list upon purchase

---
+++ includes/mailchimp/woocommerce.php
@@ -0,0 +1,39 @@
+<?php
+	define('MAILCHIMP_TAG_TO_PRODUCTS', [
+		'conf-2025-attendee' => [
+			2870,
+		],
+		'conf-2025-rec' => [
+			2872,
+		],
+	]);
+
+	add_action('woocommerce_thankyou', function( $order_id ) {
+		if ( ! $order_id ) return;
+
+		// Allow code execution only once
+		if( ! get_post_meta( $order_id, '_thankyou_action_done', true ) ) {
+			$order = wc_get_order( $order_id );
+			$order_key = $order->get_order_key();
+			$order_key = $order->get_order_number();
+
+			$isPaid = $order->is_paid();
+
+			// Loop through order items
+			foreach ( $order->get_items() as $item_id => $item ) {
+				$product = $item->get_product();
+				$product_id = $product->get_id();
+				$product_id = $item->get_name();
+				foreach ( MAILCHIMP_TAG_TO_PRODUCTS as $tag => $product_ids ) {
+					if ( in_array( $product_id, $product_ids ) ) {
+						// TODO: Add tag to Mailchimp
+						@file_put_contents( __DIR__ . '/test,log', "Adding customer associated with order $order_id to Mailchimp tag '$tag' for product ID $product_id\n", FILE_APPEND );
+					}
+				}
+			}
+
+			// Flag the action as done (to avoid repetitions on reload for example)
+			$order->update_meta_data( '_thankyou_action_done', true );
+			$order->save();
+		}
+	}, 10, 1);
