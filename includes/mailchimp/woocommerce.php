bd9f5738eaac8961026f0a9998b06c034d45725f
Serge Degtiarev <ser6722@yandex.ru>
2025-06-16 12:10:35 +1200

    debugging

--- includes/mailchimp/woocommerce.php
+++ includes/mailchimp/woocommerce.php
@@ -9,10 +9,12 @@
 	]);
 
 	add_action('woocommerce_thankyou', function( $order_id ) {
+		@file_put_contents( __DIR__ . '/test.log', "Order ID: $order_id\n---\n", FILE_APPEND );
 		if ( ! $order_id ) return;
 
 		// Allow code execution only once
 		if( ! get_post_meta( $order_id, '_thankyou_action_done', true ) ) {
+			@file_put_contents( __DIR__ . '/test.log', "Order ID: $order_id\n---\n", FILE_APPEND );
 			$order = wc_get_order( $order_id );
 			$order_key = $order->get_order_key();
 			$order_key = $order->get_order_number();
@@ -23,11 +25,10 @@
 			foreach ( $order->get_items() as $item_id => $item ) {
 				$product = $item->get_product();
 				$product_id = $product->get_id();
-				$product_id = $item->get_name();
 				foreach ( MAILCHIMP_TAG_TO_PRODUCTS as $tag => $product_ids ) {
 					if ( in_array( $product_id, $product_ids ) ) {
 						// TODO: Add tag to Mailchimp
-						@file_put_contents( __DIR__ . '/test,log', "Adding customer associated with order $order_id to Mailchimp tag '$tag' for product ID $product_id\n", FILE_APPEND );
+						@file_put_contents( __DIR__ . '/test.log', "Adding customer associated with order $order_id to Mailchimp tag '$tag' for product ID $product_id\n", FILE_APPEND );
 					}
 				}
 			}
